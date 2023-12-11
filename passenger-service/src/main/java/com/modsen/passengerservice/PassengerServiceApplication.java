ba2e62e63276b9513250e93693395258fda8de28
DmitryPasmurtsev <dima427614@gmail.com>
2023-12-11 20:50:22 +0300

    added swagger

--- passenger-service/src/main/java/com/modsen/passengerservice/PassengerServiceApplication.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/PassengerServiceApplication.java
@@ -1,9 +1,23 @@
 package com.modsen.passengerservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cab Aggregator for Modsen Company",
+                version = "1.0.0",
+                description = "Микросервис для работы с информацией о пассажирах",
+                contact = @Contact(
+                        name = "Dmitry Pasmurtsev",
+                        email = "pasmurtsevd@gmail.com"
+                )
+        )
+)
 public class PassengerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PassengerServiceApplication.class, args);
--- passenger-service/src/main/java/com/modsen/passengerservice/controller/PassengerController.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/controller/PassengerController.java
@@ -2,6 +2,8 @@ package com.modsen.passengerservice.controller;
 
 import com.modsen.passengerservice.dto.*;
 import com.modsen.passengerservice.service.PassengerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/passengers")
+@Tag(name = "Контроллер для работы с пассажирами")
 public class PassengerController {
 
     private final PassengerService passengerService;
@@ -19,30 +22,44 @@ public class PassengerController {
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+            summary = "Получение всех пассажиров"
+    )
     public ResponseEntity<?> getAllPassengers(
-            @RequestParam (required = false) Integer offset,
-            @RequestParam (required = false) Integer page,
-            @RequestParam (required = false) String field) {
-        if(offset != null && page !=null && field!=null) return ResponseEntity.ok(passengerService.getListWithPaginationAndSort(offset, page, field));
-        else if (offset != null && page != null) return  ResponseEntity.ok(passengerService.getListWithPagination(offset, page));
-        else if (field !=null) return ResponseEntity.ok(passengerService.getListWithSort(field));
+            @RequestParam(required = false) Integer offset,
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) String field) {
+        if (offset != null && page != null && field != null)
+            return ResponseEntity.ok(passengerService.getListWithPaginationAndSort(offset, page, field));
+        else if (offset != null && page != null)
+            return ResponseEntity.ok(passengerService.getListWithPagination(offset, page));
+        else if (field != null) return ResponseEntity.ok(passengerService.getListWithSort(field));
         else return ResponseEntity.ok(passengerService.getList());
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+            summary = "Получение пассажира по id"
+    )
     public PassengerResponse getPassengerById(@PathVariable Long id) {
         return passengerService.getDTOById(id);
     }
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+            summary = "Добавление пассажира"
+    )
     public StringResponse addPassenger(@RequestBody @Valid PassengerCreationRequest passengerDTO) {
         return new StringResponse("Добавлен пассажир с id " + passengerService.addPassenger(passengerDTO).getId());
     }
 
     @PatchMapping("/{id}")
     @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+            summary = "Редактирование информации о пассажире"
+    )
     public StringResponse updatePassenger(@PathVariable Long id, @Valid @RequestBody PassengerCreationRequest passengerDTO) {
         passengerService.updatePassenger(id, passengerDTO);
         return new StringResponse("Изменен пассажир с id " + id);
@@ -50,13 +67,21 @@ public class PassengerController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+            summary = "Удаление пассажира"
+    )
     public StringResponse deletePassenger(@PathVariable Long id) {
         passengerService.deletePassenger(id);
         return new StringResponse("Удален пассажир с id " + id);
     }
 
+
     @GetMapping("/{id}/rating")
+    @Operation(
+            summary = "Получение рейтинга пассажира"
+    )
     @ResponseStatus(value = HttpStatus.OK)
+    //этот метод в будущем стоит перенести в микросервис рейтингов
     public RatingResponse getPassengerRating(@PathVariable Long id) {
         return new RatingResponse(passengerService.getRatingById(id), id);
     }

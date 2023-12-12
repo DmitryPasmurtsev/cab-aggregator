3259a3a6e6b0cfdcaa2fc615cf6ebaa3f4c02f3a
DmitryPasmurtsev <dima427614@gmail.com>
2023-12-12 20:34:27 +0300

    refactoring of PassengerExceptionHandler

--- passenger-service/src/main/java/com/modsen/passengerservice/exceptions/handler/PassengerExceptionHandler.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/exceptions/handler/PassengerExceptionHandler.java
@@ -11,48 +11,35 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @ControllerAdvice
 public class PassengerExceptionHandler {
     static final String FIRST_KEY = "cause";
     static final String SECOND_KEY = "message";
 
-    @ExceptionHandler(value = {NotFoundException.class, NotCreatedException.class})
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(CustomException ex) {
+        return new ResponseEntity<>(createResponse(ex.getField(), ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {NotCreatedException.class})
     public ResponseEntity<ExceptionResponse> handleNotCreatedException(CustomException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put(FIRST_KEY, ex.getField());
-        error.put(SECOND_KEY, ex.getMessage());
-        List<Map<String, String>> list = new ArrayList<>();
-        list.add(error);
-        return new ResponseEntity<>(new ExceptionResponse(list), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createResponse(ex.getField(), ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {PropertyReferenceException.class})
     public ResponseEntity<ExceptionResponse> handlePropertyReferenceException(PropertyReferenceException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put(FIRST_KEY, ex.getPropertyName());
-        error.put(SECOND_KEY, ex.getMessage());
-        List<Map<String, String>> list = new ArrayList<>();
-        list.add(error);
-        return new ResponseEntity<>(new ExceptionResponse(list), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createResponse(ex.getPropertyName(), ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put(FIRST_KEY, "pagination");
-        error.put(SECOND_KEY, ex.getMessage());
-        List<Map<String, String>> list = new ArrayList<>();
-        list.add(error);
-        return new ResponseEntity<>(new ExceptionResponse(list), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createResponse("pagination", ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         List<Map<String, String>> responseList = new ArrayList<>();
         Map<String, String> responseMap = new HashMap<>();
         ex.getFieldErrors().forEach(err -> {
@@ -62,5 +49,13 @@ public class PassengerExceptionHandler {
         });
         ExceptionResponse response = new ExceptionResponse(responseList);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    private ExceptionResponse createResponse(String firstValue, String secondValue) {
+        Map<String, String> error = new HashMap<>();
+        error.put(FIRST_KEY, firstValue);
+        error.put(SECOND_KEY, secondValue);
+        List<Map<String, String>> list = Collections.singletonList(error);
+        return new ExceptionResponse(list);
     }
 }

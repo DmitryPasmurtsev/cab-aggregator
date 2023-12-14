ed577d94dde75c9f41369e440e4cc0edf4592fa0
DmitryPasmurtsev <dima427614@gmail.com>
2023-12-14 13:56:15 +0300

    added private access modifiers

--- passenger-service/src/main/java/com/modsen/passengerservice/exceptions/CustomException.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/exceptions/CustomException.java
@@ -1,10 +1,13 @@
 package com.modsen.passengerservice.exceptions;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class CustomException extends RuntimeException{
-    String field;
+    final String field;
 
     public CustomException(String field, String message) {
         super(message);

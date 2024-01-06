6a363f3bd8e3baaa12dc9da1ae612a6132ff7291
DmitryPasmurtsev <dima427614@gmail.com>
2024-01-06 16:35:08 +0300

    imports optimization

--- driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
+++ driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
@@ -3,15 +3,23 @@ package com.modsen.driverservice.controller;
 import com.modsen.driverservice.dto.request.DriverCreationRequest;
 import com.modsen.driverservice.dto.response.DriverResponse;
 import com.modsen.driverservice.dto.response.DriversListResponse;
-import com.modsen.driverservice.dto.response.RatingResponse;
-import com.modsen.driverservice.dto.response.StringResponse;
 import com.modsen.driverservice.service.DriverService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("api/v1/drivers")

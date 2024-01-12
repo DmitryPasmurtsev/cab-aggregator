b12c07cd16267efc2faceb8cea17266c9b79d54f
DmitryPasmurtsev <dima427614@gmail.com>
2024-01-12 22:55:21 +0300

    changed service code

--- passenger-service/src/main/java/com/modsen/passengerservice/dto/response/PassengersListResponse.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/dto/response/PassengersListResponse.java
@@ -2,6 +2,7 @@ package com.modsen.passengerservice.dto.response;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Getter
 @Builder
+@EqualsAndHashCode
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PassengersListResponse {
     Integer page;
--- passenger-service/src/main/java/com/modsen/passengerservice/exceptions/response/ExceptionResponse.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/exceptions/response/ExceptionResponse.java
@@ -3,6 +3,8 @@ package com.modsen.passengerservice.exceptions.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.Map;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ExceptionResponse {
     List<Map<String, String>> errorMessage;
